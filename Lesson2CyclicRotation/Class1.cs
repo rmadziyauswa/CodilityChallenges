38ce1658972a117842b3cf5cc6e7ef65aa8cd232
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-03 23:34:08 +0200

    Init Lesson 2 And Unit Test Project

---
+++ Lesson2CyclicRotation/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2CyclicRotation
+{
+    public class Solution
+    {
+        public int[] solution(int[] A, int K)
+        {
+            var result = A.ToList();
+            var lastIndex = A.Length - 1;
+            for (int i = 0; i < K; i++)
+            {
+                var elementToMove = result[lastIndex];
+                result.RemoveAt(lastIndex);
+                result = result.Prepend(elementToMove).ToList();
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ UnitTestProject/CyclicRotationUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Lesson2CyclicRotation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CyclicRotationUnitTest
+    {
+        [TestMethod]
+        public void CyclicRotationUnitTest_Must_Always_Pass()
+        {
+            int[] array1 = new int[] { 3, 8, 9, 7, 6 };
+            int k1 = 3;
+            int[] expectedAns1 = new int[] { 9, 7, 6, 3, 8 };
+
+            Solution sol = new Solution();
+            var actualAnswer1 = sol.solution(array1, k1);
+            Assert.AreEqual<int[]>(expectedAns1, actualAnswer1);
+
+        }
+    }
+}
