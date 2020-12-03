a335947be7159c05978a9d6398ef8777f1e0c217
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-03 14:29:04 +0200

    Init

---
+++ Lesson1_BinaryGap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1_BinaryGap
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
