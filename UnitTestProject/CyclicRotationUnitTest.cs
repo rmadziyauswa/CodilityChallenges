a6ebb6ecd3ef799948a1870fc45fde5d28eae943
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-04 00:06:45 +0200

    refactored unit test method

--- UnitTestProject/CyclicRotationUnitTest.cs
+++ UnitTestProject/CyclicRotationUnitTest.cs
@@ -14,29 +14,26 @@ namespace UnitTestProject
         {
             //Arrange
             Solution sol = new Solution();
-
-            var array1 = new int[] { 3, 8, 9, 7, 6 };
-            var k1 = 3;
-            var expectedAns1 = new int[] { 9, 7, 6, 3, 8 };
-
-            var array2 = new int[] { 0, 0, 0 };
-            var k2 = 1;
-            var expectedAns2 = new int[] { 0, 0, 0 };
-
-            var array3 = new int[] { 1, 2, 3, 4 };
-            var k3 = 4;
-            var expectedAns3 = new int[] { 1, 2, 3, 4 };
+            var numTests = 3;
+            var listOfArrays = new List<int[]> {
+                new int[] { 3, 8, 9, 7, 6 },
+                new int[] { 0, 0, 0 },
+                new int[] { 1, 2, 3, 4 }
+            };
+            var listOfKs = new List<int> { 3,1,4};
+            var listOfExpectedAnswers = new List<int[]> {
+                new int[] { 9, 7, 6, 3, 8 },
+                new int[] { 0, 0, 0 },
+                new int[] { 1, 2, 3, 4 }
+            };
 
             //Act
-            var actualAnswer1 = sol.solution(array1, k1);
-            var actualAnswer2 = sol.solution(array2, k2);
-            var actualAnswer3 = sol.solution(array3, k3);
-
-            //Assert
-            Assert.IsTrue(expectedAns1.SequenceEqual(actualAnswer1));
-            Assert.IsTrue(expectedAns2.SequenceEqual(actualAnswer2));
-            Assert.IsTrue(expectedAns3.SequenceEqual(actualAnswer3));
-
+            for (int i = 0; i < numTests; i++)
+            {
+                var actualAnswer = sol.solution(listOfArrays[i], listOfKs[i]);
+                //Assert
+                Assert.IsTrue(listOfExpectedAnswers[i].SequenceEqual(actualAnswer));
+            }
         }
     }
 }
