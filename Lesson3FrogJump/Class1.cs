5f7067419132d87f7c50a33eecb14780dea753bc
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-04 14:11:24 +0200

    Set Up of NUnit project for .net core tests

---
+++ Lesson3FrogJump/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson3FrogJump
+{
+    public class Solution {
+        public int solution(int X, int Y, int D)
+        {
+            return 0;
+        }
+
+    }
+}
