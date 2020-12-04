766ed28c5e384f0f75da3bf20fec6254e03fa31c
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-04 15:38:40 +0200

    Lesson4 Frog RiverOne solution

---
+++ Lesson4FrogRiverOne/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson4FrogRiverOne
+{
+    public class Solution
+    {
+        public int solution(int X, int[] A)
+        {
+            var listOfIndicesWithX = Enumerable.Range(0, A.Length - 1).Where(i => A[i] == X);
+            var requiredInts = Enumerable.Range(1, X).ToList<int>();
+            foreach (var index in listOfIndicesWithX)
+            {
+                var listToIndex = A.Where((v, i) => i <= index).ToList<int>();
+                var countOfRequiredInts = listToIndex.Where(v => requiredInts.Contains(v)).Distinct().Count();
+                if(countOfRequiredInts == requiredInts.Count())
+                {
+                    return index;
+                }
+                
+            }
+            return -1;
+        }
+    }
+}
---
+++ NUnitTests/Lesson4FrogRiverOne.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lesson4FrogRiverOne;
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+    public class Lesson4FrogRiverOne
+    {
+        [Test]
+        public void Test1()
+        {
+            var sol = new Solution();
+            var listOfXs = new List<int> { 5 };
+            var listOfArrays = new List<int[]> { new int[] {1,3,1,4,2,3,5,4 } };
+            var listOfExpecteds = new List<int> { 6 };
+            for (int i = 0; i < listOfXs.Count; i++)
+            {
+                var actualAnswer = sol.solution(listOfXs[i], listOfArrays[i]);
+                Assert.AreEqual(actualAnswer, listOfExpecteds[i]);
+            }
+        }
+    }
+}
