9ebc185ce79cc8f9ae9ccadd10de853e29716b08
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-04 14:30:22 +0200

    Lesson 3 solution for Frog Jump and Unit test

--- Lesson3FrogJump/Class1.cs
+++ Lesson3FrogJump/Class1.cs
@@ -5,6 +5,10 @@ namespace Lesson3FrogJump
     public class Solution {
         public int solution(int X, int Y, int D)
         {
+            if(X <= Y)
+            {
+                return (int)Math.Ceiling(((double)Y - X) / D);
+            }
             return 0;
         }
 
